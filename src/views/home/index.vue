1c56388bcd51f535ea5d875e2833a272b3894617
TaoLoading <1377226380@qq.com>
2022-06-20 11:46:24 +0800

    集成ElementPlus

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 import { useRouter, useRoute } from 'vue-router'
+import { h, getCurrentInstance } from 'vue'
 
 const router = useRouter()
 const route = useRoute()
 console.log(route.params)
+const { proxy }: any = getCurrentInstance()
+
+proxy.$message({
+  message: h('p', null, [
+    h('span', null, 'Message can be '),
+    h('i', { style: 'color: teal' }, 'VNode'),
+  ]),
+})
 </script>
 
 <template>

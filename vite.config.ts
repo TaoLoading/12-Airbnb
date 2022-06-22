092920908d7b0a43f70efb52b1cbed1c4535fbac
TaoLoading <1377226380@qq.com>
2022-06-22 09:29:08 +0800

    清除i18n所报警告

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
   plugins: [
     vue(),
     AutoImport({

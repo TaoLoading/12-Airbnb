d48ec63e05b1bc0a2f9ef828853e8a21a2834bba
TaoLoading <1377226380@qq.com>
2022-07-05 17:01:29 +0800

    配置打包与运行指令

--- server.js
+++ server.js
@@ -6,38 +6,67 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const { createServer: createViteServer } = require('vite')
+const serverStatic = require('serve-static')
+// 判断是否为生产环境
+const isProd = (process.env.NODE_ENV === 'production')
 
 async function createServer() {
   const app = express()
-
   // 以中间件模式创建 Vite 应用，这将禁用 Vite 自身的 HTML 服务逻辑
   // 并让上级服务器接管控制
   const vite = await createViteServer({
     server: { middlewareMode: true },
     appType: 'custom'
   })
-  // 使用 vite 的 Connect 实例作为中间件
-  app.use(vite.middlewares)
+
+  if (!isProd) {
+    // 开发环境下
+
+    // 使用 vite 的 Connect 实例作为中间件
+    app.use(vite.middlewares)
+  } else {
+    // 生产环境下
+
+    app.use(serverStatic(path.resolve(__dirname, 'dist/client')))
+  }
 
   app.use('*', async (req, res, next) => {
     const url = req.originalUrl
 
     try {
-      // 1. 读取 index.html
-      let template = fs.readFileSync(
-        path.resolve(__dirname, 'index.html'),
-        'utf-8'
-      )
+      let template
+      let render
 
-      // 2. 应用 Vite HTML 转换。这将会注入 Vite HMR 客户端，
-      //    同时也会从 Vite 插件应用 HTML 转换。
-      //    例如：@vitejs/plugin-react 中的 global preambles
-      template = await vite.transformIndexHtml(url, template)
+      if (!isProd) {
+        // 开发环境下
 
-      // 3. 加载服务器入口。vite.ssrLoadModule 将自动转换
-      //    你的 ESM 源码使之可以在 Node.js 中运行！无需打包
-      //    并提供类似 HMR 的根据情况随时失效。
-      const { render } = await vite.ssrLoadModule('./src/entry-server.ts')
+        // 1. 读取 index.html
+        template = fs.readFileSync(
+          path.resolve(__dirname, 'index.html'),
+          'utf-8'
+        )
+
+        // 2. 应用 Vite HTML 转换。这将会注入 Vite HMR 客户端，
+        //    同时也会从 Vite 插件应用 HTML 转换。
+        //    例如：@vitejs/plugin-react 中的 global preambles
+        template = await vite.transformIndexHtml(url, template)
+
+        // 3. 加载服务器入口。vite.ssrLoadModule 将自动转换
+        //    你的 ESM 源码使之可以在 Node.js 中运行！无需打包
+        //    并提供类似 HMR 的根据情况随时失效。
+        render = (await vite.ssrLoadModule('./src/entry-server.ts')).render
+      } else {
+        // 生产环境下
+
+        // 1. 读取 index.html
+        template = fs.readFileSync(
+          path.resolve(__dirname, 'dist/client/index.html'),
+          'utf-8'
+        )
+
+        // 2. 加载服务器入口
+        render = require('./dist/server/entry-server.js').render
+      }
 
       // 4. 渲染应用的 HTML。这假设 entry-server.js 导出的 `render`
       //    函数调用了适当的 SSR 框架 API。
@@ -50,16 +79,16 @@ async function createServer() {
       // 6. 返回渲染后的 HTML。
       res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
     } catch (e) {
+      console.log('node出现错误是', e)
       // 如果捕获到了一个错误，让 Vite 来修复该堆栈，这样它就可以映射回
       // 你的实际源码中。
       vite.ssrFixStacktrace(e)
-      console.log('node出现错误是', e)
       next(e)
     }
   })
 
   app.listen(5173, () => {
-    console.log('node server 运行 localhost:5173')
+    console.log('node server 运行 localhost:5173', isProd ? '生产环境' : '开发环境')
   })
 }
 
--- src/components/layout/commonHeader.vue
+++ src/components/layout/commonHeader.vue
@@ -23,8 +23,8 @@
 
 <script lang="ts" setup>
 import { getCurrentInstance, ref } from 'vue'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import en from 'element-plus/es/locale/lang/en'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+import en from 'element-plus/lib/locale/lang/en'
 import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
 import { fetchLanguageApi } from '../../api/layout'

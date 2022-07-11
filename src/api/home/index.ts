91fdadd81e167550c8fd7b7234beabe0f1a4f163
TaoLoading <1377226380@qq.com>
2022-07-11 16:24:16 +0800

    修改接口传参为必传

--- src/api/home/index.ts
+++ src/api/home/index.ts
@@ -2,6 +2,6 @@ import { http } from '../../utils/http'
 import { IResultOr as IResult, IRoomListParams } from '../interface'
 
 // 真实接口———首页房屋列表接口
-export function fetchRoomList(params?: IRoomListParams): Promise<IResult> {
+export function fetchRoomList(params: IRoomListParams): Promise<IResult> {
   return http.httpRequestGet('/api/room/room/getRoomList', params)
 }

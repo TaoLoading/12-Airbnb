4545e276106a84452c7c3778d6fb6c1d15ec27f5
TaoLoading <1377226380@qq.com>
2022-07-12 16:19:42 +0800

    去除多余代码

--- src/api/home/index.ts
+++ src/api/home/index.ts
@@ -1,7 +1,7 @@
 import { http } from '../../utils/http'
 import { IResultOr as IResult, IRoomListParams } from '../interface'
 
-// 获取房屋列表
+// 查询房屋列表
 export function fetchRoomList(params: IRoomListParams): Promise<IResult> {
   console.log('进入房屋列表接口')
   return http.httpRequestGet('/api/room/room/getRoomList', params)
